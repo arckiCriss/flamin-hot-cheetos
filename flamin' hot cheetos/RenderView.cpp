f272bcafed483400d3a2dacef44e94100713b9c4
Epik <xboxjames123@gmail.com>
2016-10-28 16:35:16 -0700

    hopefully this fixes the crash...

--- flamin' hot cheetos/RenderView.cpp
+++ flamin' hot cheetos/RenderView.cpp
@@ -2,10 +2,10 @@
 
 RenderView_t originalRenderView;
 
-void __stdcall RenderView( CViewSetup& view, CViewSetup& hudViewSetup, int clearFlags, int whatToDraw )
+void __fastcall RenderView( void* thisptr, void*, CViewSetup& view, CViewSetup& hudViewSetup, int clearFlags, int whatToDraw )
 {
 	if ( cvar::misc_overridefov > 0 )
 		view.fovViewmodel += cvar::misc_overridefov;
 
-	originalRenderView( interfaces::viewRender, view, hudViewSetup, clearFlags, whatToDraw );
+	originalRenderView( thisptr, view, hudViewSetup, clearFlags, whatToDraw );
 }
